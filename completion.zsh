0df2b3f84fc447fdddbd0af7d5fb598057f53b0a
Josh Wainwright <wainwright.ja@gmail.com>
2013-10-22 20:33:09 +0100

    Improved completion for zsh

--- completion.zsh
+++ completion.zsh
@@ -12,10 +12,10 @@ if [[ "$TERM" == 'dumb' ]]; then
 fi
 
 # Add zsh-completions to $fpath.
-fpath=("~/.zsh/completion/src" $fpath)
+fpath=("$HOME/.zsh/completion/src/" $fpath)
 
 # Load and initialize the completion system ignoring insecure directories.
-autoload -Uz compinit && compinit -i
+autoload -Uz compinit && compinit
 
 #
 # Options
@@ -36,7 +36,7 @@ unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 
 # Use caching to make completion for cammands such as dpkg and apt usable.
 zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
+zstyle ':completion::complete:*' cache-path "$HOME/.zsh/.zcompcache"
 
 # Case-insensitive (all), partial-word, and then substring completion.
 if zstyle -t ':prezto:module:completion:*' case-sensitive; then
