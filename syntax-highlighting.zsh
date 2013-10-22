7554e49930306e15f6dc73530b32e89abc7ef794
Josh Wainwright <wainwright.ja@gmail.com>
2013-10-22 20:34:18 +0100

    syntax highlighted cat

--- syntax-highlighting.zsh
+++ syntax-highlighting.zsh
@@ -2,3 +2,10 @@
 source "/home/josh/.zsh/syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+
+function pygmentize_cat {
+  for arg in "$@"; do
+    pygmentize -g "${arg}" 2> /dev/null || /bin/cat "${arg}"
+  done
+}
+command -v pygmentize > /dev/null && alias cat=pygmentize_cat
