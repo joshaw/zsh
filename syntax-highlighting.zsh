8cb3c81da8724607c48865865ec0151a5f0d9e2a
Josh Wainwright <josh.wainwright@ldra.com>
2015-01-12 14:01:17 +0000

    Remove syntax highlighting completely for cygwin

--- syntax-highlighting.zsh
+++ syntax-highlighting.zsh
@@ -1,16 +1,20 @@
+# Created:  Wed 15 Oct 2014
+# Modified: Mon 12 Jan 2015
+# Author:   Josh Wainwright
+# Filename: syntax-highlighting.zsh
+#
 # Source module files.
-source "${HOME}/.zsh/syntax/zsh-syntax-highlighting.zsh"
 
 is_cygwin() {
 	# Check we're running under cygwin
-	local uname=`uname -s`
-	[[ $uname[0,6] == 'CYGWIN' ]]
+	# local uname=`uname -s`
+	# [[ $uname[0,6] == 'CYGWIN' ]]
+	[[ $OSTYPE == 'cygwin' ]]
 }
 
-if is_cygwin; then
-	ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
-else
+if ! is_cygwin; then
 	ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root line)
+	source "${HOME}/.zsh/syntax/zsh-syntax-highlighting.zsh"
 fi
 
 zsh_timing_function ${(%):-%N}
