94bfbc7f5d07fc05320d49901903fa90e018ff6d
Josh Wainwright <josh.wainwright@ldra.com>
2015-01-21 17:03:23 +0000

    Add aliases specific to ldra

---
+++ aliases_ldra.zsh
@@ -0,0 +1,22 @@
+# Created:  Tue 20 Jan 2015
+# Modified: Wed 21 Jan 2015
+# Author:   Josh Wainwright
+# Filename: aliases_ldra.zsh
+
+function ldra-{{start,stop}_licenseserver,re-read_licensefile}() {
+	name=$0
+	cmd=${name#ldra-}
+	cd -q ~/LDRA_flexlm/
+	echo $cmd
+	./${cmd}.sh
+	cd -q -
+}
+
+alias ldralauncher='~/ldra/launcher/ldralauncher'
+
+function ldra-{testbed,tbvision,tbmanager,tbrun}-{Ada,C_C++}-{944,946}() {
+	name=$0
+	p=(${(s/-/)name})
+	cp ~/.ldra/Testbed.ini.$p[3]$p[4] ~/.ldra/Testbed.ini
+	~/LDRA_Toolsuite/$p[4]/$p[3]/$p[2]
+}
