6f6d5b8c5ef787f028634eb198af9cad1164238c
Josh Wainwright <josh.wainwright@ldra.com>
2015-02-10 17:20:54 +0000

    Add async git information

--- prompt.zsh
+++ prompt.zsh
@@ -1,5 +1,5 @@
 # Created:  Tue 15 Oct 2013
-# Modified: Wed 21 Jan 2015
+# Modified: Mon 09 Feb 2015
 # Author:   Josh Wainwright
 # Filename: prompt.zsh
 #
@@ -24,48 +24,53 @@ autoload -Uz promptinit && promptinit
 # fastest possible way to check if repo is dirty
 git_dirty() {
 	# check if we're in a git repo
-	command git rev-parse --is-inside-work-tree &>/dev/null || return
+	[[ "$(command git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || return
+	echo -n " $(git rev-parse --abbrev-ref HEAD) "
 	# check if it's dirty
-	command git diff --quiet --ignore-submodules HEAD &>/dev/null
+	command test -n "$(git status --porcelain --ignore-submodules -unormal)"
 
-	(($? == 1)) && echo '*'
+	(($? == 0)) && echo '*'
 }
 
-# displays the exec time of the last command if set threshold was exceeded
-cmd_exec_time() {
-	local stop=$(date +%s)
-	local start=${cmd_timestamp:-$stop}
-	integer elapsed=$stop-$start
-#	(($elapsed > ${PURE_CMD_MAX_EXEC_TIME:=5})) && echo ${elapsed}s
-	echo ${elapsed}s
-}
-
-p_preexec() {
-	cmd_timestamp=$(date +%s)
+# string length ignoring ansi escapes
+string_length() {
+	echo ${#${(S%%)1//(\%([KF1]|)\{*\}|\%[Bbkf])}}
 }
 
 p_precmd() {
-	# git info
-	vcs_info
+	# shows the full path in the title
+	print -Pn '\e]0;%~\a'
 
-	print -P "\n%F{cyan}%~%F{8}$vcs_info_msg_0_$(git_dirty) $p_username%f %F{yellow}$(cmd_exec_time)%f"
+	local p_preprompt="\n%F{cyan}%~%F{8} $p_username%f"
+	print -P $p_preprompt
 
 	# reset value since `preexec` isn't always triggered
 	unset cmd_timestamp
+
+	# check async if there is anything to pull
+	{
+		# check if we're in a git repo
+		[[ "$(command git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] &&
+		# make sure working tree is not $HOME
+		[[ "$(command git rev-parse --show-toplevel)" != "$HOME" ]] &&
+		# check check if there is anything to pull
+		command git fetch &>/dev/null &&
+		# check if there is an upstream configured for this branch
+		command git rev-parse --abbrev-ref @'{u}' &>/dev/null && {
+			local arrows=''
+			(( $(command git rev-list --right-only --count HEAD...@'{u}' 2>/dev/null) > 0 )) && arrows='v'
+			(( $(command git rev-list --left-only --count HEAD...@'{u}' 2>/dev/null) > 0 )) && arrows+='^'
+			print -Pn "\e7\e[A\e[1G\e[`string_length $p_preprompt`C%F{8}$(git_dirty)%f%F{cyan}${arrows}%f\e8"
+		}
+	} &!
 }
 
 prompt_setup() {
 	prompt_opts=(cr subst percent)
 
 	autoload -Uz add-zsh-hook
-	autoload -Uz vcs_info
 
 	add-zsh-hook precmd p_precmd
-	add-zsh-hook preexec p_preexec
-
-	zstyle ':vcs_info:*' enable git svn
-	zstyle ':vcs_info:git*' formats ' %b'
-	zstyle ':vcs_info:git*' actionformats ' %b|%a'
 
 	# show username@host if logged in through SSH
 	[[ "$SSH_CONNECTION" != '' ]] && p_username='%n@%m '
